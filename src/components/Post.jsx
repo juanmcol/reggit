6df6ff36b72c82a625930306c09c0ff362704c94
juanmcol <jmcolina1996@gmail.com>
2026-05-21 15:09:27 -0500

    Create icons for votes, and comments. Move authors to the top of posts.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,10 @@ export default function Post({post}) {
     return (
         <div className="post" key={post.id}>
             <div className="header">
-                <p className="creation">r/{post.subreddit} - {Math.floor((time - post.created)/3600)} hr. ago</p>
+                <div className="info-container">
+                    <p className="creation">r/{post.subreddit} - {Math.floor((time - post.created)/3600)} hr. ago</p>
+                    <p className="author">by {post.author}</p>
+                </div>
                 <h3 className="title">{post.title}</h3>
             </div>
             <div className="content">
@@ -11,9 +14,24 @@ export default function Post({post}) {
                 {post.post_hint === "hosted:video" && <video controls width="400" height="400" src={post.secure_media.reddit_video.fallback_url}></video>}
             </div>
             <div className="details">
-                <p className="upvotes">{post.ups} upvotes</p>
-                <p className="comments">{post.num_comments} comments</p>
-                <p className="author">by {post.author}</p>
+                <div className="upvotes-container">
+                    <div className="upvote-icon">
+                        <i className="upvote-arrow" aria-label="upvote"></i>
+                        <i className="upvote-square" aria-label="upvote"></i>
+                    </div>
+                    <p className="upvote-count">{post.ups}</p>
+                    <div className="downvote-icon">
+                        <i className="downvote-square" aria-label="downvote"></i>
+                        <i className="downvote-arrow" aria-label="downvote"></i>
+                    </div>
+                </div>
+                <div className="comments-container">
+                    <div className="comment-icon">
+                        <i className="comment-bubble" aria-label="comments"></i>
+                        <i className="comment-arrow" aria-label="comments"></i>
+                    </div>
+                    <p className="comments">{post.num_comments}</p>
+                </div>
             </div>
         </div>
     )
